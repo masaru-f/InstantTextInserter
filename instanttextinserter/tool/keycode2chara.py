c252019b12fc6dcabedc8019d99538e4b0ee415e
shouh <shouhpublic@gmail.com>
2013-12-21 10:47:41 +0900

    短縮形として入力できる文字全てを確かめるスニペットファイルを生成するスクリプト

---
+++ instanttextinserter/tool/keycode2chara.py
@@ -0,0 +1,98 @@
+
+"""
+SnippetManager の keycode2chara を作るコード.
+標準出力に出てきたものを丸々コピペするだけ.
+
+"""
+
+INVALID_CHAR = "?"
+def keycode2chara(keycode):
+    c = INVALID_CHAR
+    i = keycode
+
+    if i>=65 and i<=90:
+        c = chr(i)
+    elif i>=48 and i<=57:
+        c = str(i-48)
+    elif i>=96 and i<=105:
+        c = str(i-96)
+    elif i==9:
+        c = "\\t"
+    elif i==32:
+        c = " "
+    elif i==186: # chr しても対応する文字が出てこないので仕方なく列挙...
+        c = ":"
+    elif i==187:
+        c = ";"
+    elif i==188:
+        c = ","
+    elif i==189:
+        c = "-"
+    elif i==190:
+        c = "."
+    elif i==191:
+        c = "/"
+    elif i==192:
+        c = "@"
+    elif i==219:
+        c = "["
+    elif i==220:
+        c = "\\\\"
+    elif i==221:
+        c = "]"
+    elif i==222:
+        c = "^"
+    elif i==226:
+        c = "\\\\"
+
+    return c
+
+class FileWriter:
+    def __init__(self):
+        return
+
+    def write(self, filename, content):
+        """
+        改行は自動的に付加する.
+        @param content 書き込む内容のシーケンス
+        """
+        _content = content[:]
+
+        for i in range(len(_content)):
+            # os.linesep だと改行が二つ挿入される.
+            # windows 限定アプリなので \n でいいや.
+            #_content[i] += os.linesep
+            _content[i] += "\n"
+
+        fileobj = open(filename, "w")
+        ret = fileobj.writelines(_content)
+        fileobj.close()
+
+# --------------------------------
+
+def printlist():
+    s = ""
+    for i in range(256):
+        c = keycode2chara(i)
+        s = s + "'" + c + "',"
+    print s
+
+def create_file4test():
+    """
+    入力した文字を二つ入力するスニペットファイルを生成.
+    これを使って各文字の入力が行えるかを手動で確かめる.
+    """
+    ls = []
+    for i in range(256):
+        c = keycode2chara(i)
+        if c==INVALID_CHAR:
+            continue
+        c = c.replace("\\t", "\t").replace("\\\\", "\\")
+        ls.append("===" + c + "===")
+        ls.append(c+c)
+
+    writer = FileWriter()
+    writer.write("charatest.txt", ls)
+
+printlist()
+create_file4test()
