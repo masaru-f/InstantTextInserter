ca7439500b5b1074780eb1b07398a82187396f3d
shouh <shouhpublic@gmail.com>
2014-02-08 10:48:19 +0900

    複数のホットキー設定を管理する HotkeyManager クラスを追加

--- instanttextinserter/util_win/hotkey.py
+++ instanttextinserter/util_win/hotkey.py
@@ -1,25 +1,11 @@
 # encoding: shift-jis
 
 import ctypes
+import win32api
 import win32con
 
 import keycode
 
-"""
-hotkeyconfig in util.win
-- hwnd
-- index
-- modifier
-- key
-- callback
-- ctor(hwnd, index)
-- register_hotkey(modifier, key)
- - RegisterHotkey を呼び出す
- - 成功したら, 自身のメンバ変数を更新
-- set_callback(callback)
-- unregister_hotkey()
- - UnregisterHotkey を呼び出す
-"""
 class HotkeyConfig:
     """
     ホットキー一つ分の設定情報と振る舞いを持つ.
@@ -56,6 +42,9 @@ class HotkeyConfig:
 
         return True
 
+    def get_index(self):
+        return self._index
+
     def set_callback(self, callback):
         self._callback = callback
 
@@ -89,9 +78,83 @@ hotkeymanager in util.win
  - @note も少し間引いてもいいかも on_hotkey(index, modifier, key)
  - index に該当する callback を探し, それを実行
 """
-class IniLoader:
-    def __init__(self):
-        return
+class HotkeyManager:
+    """
+    複数のホットキー設定を管理.
+    各設定は名前(key-value の key)で識別.
+    """
+    def __init__(self, hwnd):
+        self._map = {}
+        self._hwnd = hwnd
+        self._max_index = 0 # 使用可能なホットキー識別子の下限
+
+    def register_hotkey(self, name, modifier, key):
+        """
+        @todo エラー時の設計
+        @retval False ホットキー登録に失敗
+        """
+        hotkey_config = None
+        try:
+            hotkey_config = self._map[name]
+        except KeyError:
+            pass
+
+        if hotkey_config:
+            is_hotkey_valid = hotkey_config.register_hotkey(modifier, key)
+            return is_hotkey_valid
+
+        hotkey_config = HotkeyConfig(self._hwnd, self._max_index)
+        self._max_index = self._max_index + 1
+
+        is_hotkey_valid = hotkey_config.register_hotkey(modifier, key)
+        if not(is_hotkey_valid):
+            return False
+
+        self._map[name] = hotkey_config
+
+    def register_callback(self, name, callback):
+        hotkey_config = None
+        try:
+            hotkey_config = self._map[name]
+        except KeyError:
+            return
+
+        hotkey_config.set_callback(callback)
+
+    def unregister_all(self):
+        hotkey_config_list = self._map.keys()
+        for hotkey_config in hotkey_config_list:
+            hotkey_config.unregister_hotkey()
+
+    def on_hotkey(self, hwnd, message, wparam, lparam):
+        """
+        ホットキー押下時のコールバック関数本体.
+        押下されたホットキーに対応するコールバック関数を呼び出す.
+
+        @param wparam ホットキーの識別子
+        @param lparam 下位WORDはmodifier, 上位WORDはキーコード
+        """
+        pushee_index = wparam
+        pushee_modifier = win32api.LOWORD(lparam)
+        pushee_key = win32api.HIWORD(lparam)
+
+        hotkey_config_list = self._map.keys()
+
+        for hotkey_config in hotkey_config_list:
+            index = hotkey_config.get_index()
+            if index != pushee_index:
+                continue
+
+            callback = hotkey_config.get_callback()
+            if not(callable(callback)):
+                # 正しいコールバック関数が入ってないとここに来る.
+                # これはプログラマ側のエラー.
+                # @todo プログラムエラーであることがわかるようにしたい.
+                # @todo コールバック関数の正しさはどこで検証する? \n
+                #       ここ or セットするところ のいずれかだろう.
+                return
+
+            callback()
 
 if __name__ == '__main__':
     pass
