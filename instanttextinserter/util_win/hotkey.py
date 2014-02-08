8dce80a8691ae245dd860fb24d73a7e4b91c6f15
shouh <shouhpublic@gmail.com>
2014-02-08 10:10:50 +0900

    ホットキー設定情報を表現する HotkeyConfig クラスを追加

---
+++ instanttextinserter/util_win/hotkey.py
@@ -0,0 +1,93 @@
+# encoding: shift-jis
+
+import ctypes
+import win32con
+
+import keycode
+
+"""
+hotkeyconfig in util.win
+- hwnd
+- index
+- modifier
+- key
+- callback
+- ctor(hwnd, index)
+- register_hotkey(modifier, key)
+ - RegisterHotkey を呼び出す
+ - 成功したら, 自身のメンバ変数を更新
+- set_callback(callback)
+- unregister_hotkey()
+ - UnregisterHotkey を呼び出す
+"""
+class HotkeyConfig:
+    """
+    ホットキー一つ分の設定情報と振る舞いを持つ.
+
+    """
+    def __init__(self, hwnd, index):
+        self._hwnd = hwnd
+        self._index = index
+        self._modifier = None
+        self._key = None
+        self._callback = None
+
+    def register_hotkey(self, modifier, key):
+        """
+        新しいホットキー登録を行うと, 以前登録分は上書きされる.
+        これは RegisterHotKey の仕様で, index を固定しているため.
+
+        @param modifier win32con の MOD_HOGE の組み合わせ
+        @param key 仮想キーコード
+        """
+        is_hotkey_valid = ctypes.windll.user32.RegisterHotKey(
+            self._hwnd,
+            self._index,
+            modifier,
+            key
+        )
+        if not(is_hotkey_valid):
+            # @todo 戻り値設計 win32api.GetLastError() を使うか?
+            return
+        # ホットキー登録成功後に更新する.
+        self._modifier = modifier
+        self._key = key
+
+    def set_callback(self, callback):
+        self._callback = callback
+
+    def get_callback(self):
+        return self._callback
+
+    def unregister_hotkey(self):
+        could_unregister = ctypes.windll.user32.UnregisterHotKey(
+            self._hwnd,
+            self._index
+        )
+        if not(could_unregister):
+            # @todo 戻り値設計 win32api.GetLastError() を使うか?
+            return
+
+"""
+hotkeymanager in util.win
+- map
+- hwnd
+- maxindex
+ - 初期値1. 一つ hotkeyconfig を作るたびにインクリメント.
+- ctor(hwnd)
+ - map["reload"] = new HotkeyConfig(hwnd, 1)
+ - map["open_snippet_folder"] = new HotkeyConfig(hwnd, 2)
+ - …
+- register_hotkey(name, modifier, key)
+ - map[name] が無ければ新規, あればそれを使う.
+- register_callback(name, callback)
+- unregister_all()
+- on_hotkey(hwnd, message, wparam, lparam)
+ - @note も少し間引いてもいいかも on_hotkey(index, modifier, key)
+ - index に該当する callback を探し, それを実行
+"""
+class IniLoader:
+    def __init__(self):
+        return
+
+if __name__ == '__main__':
