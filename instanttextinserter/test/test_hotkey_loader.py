65e7c026f043e5dc36005a84d885b05bc90b3bdb
shouh <shouhpublic@gmail.com>
2014-02-09 16:53:18 +0900

    テストコードをリファクタリング. メソッド移動とメソッド名変更

--- instanttextinserter/test/test_hotkey_loader.py
+++ instanttextinserter/test/test_hotkey_loader.py
@@ -16,49 +16,52 @@ class HotkeyEntryTest(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def assertEntry(self, line, exp_name, exp_mod, exp_key):
-        hotkey_entry = hotkey_loader.HotkeyEntry(line)
-        self.assertEqual(hotkey_entry._name, exp_name)
-        self.assertEqual(hotkey_entry._modifier, exp_mod)
-        self.assertEqual(hotkey_entry._keycode, exp_key)
-
     def test_HotkeyEntry(self):
         """
-        正常系は簡単にテスト. 余力あらば観点を追加するといい.
+        既にテスト済の他モジュールから流用してるため,
+        テストは超ざっくりに留めてある.
+        問題が出たら改めて追加する予定.
         """
+
+        def assert_entry(line, exp_name, exp_mod, exp_key):
+            hotkey_entry = hotkey_loader.HotkeyEntry(line)
+            self.assertEqual(hotkey_entry._name, exp_name)
+            self.assertEqual(hotkey_entry._modifier, exp_mod)
+            self.assertEqual(hotkey_entry._keycode, exp_key)
+
         testname = "hoge"
 
         # 各修飾キーを正しく変換できる
-        self.assertEntry(
+        assert_entry(
             testname + "," + "a, k",
             testname, win32con.MOD_ALT, keycode.K
         )
-        self.assertEntry(
+        assert_entry(
             testname + "," + "c, insert",
             testname, win32con.MOD_CONTROL, keycode.INSERT
         )
-        self.assertEntry(
+        assert_entry(
             testname + "," + "s, enter",
             testname, win32con.MOD_SHIFT, keycode.ENTER
         )
-        self.assertEntry(
+        assert_entry(
             testname + "," + "w, esc",
             testname, win32con.MOD_WIN, keycode.ESC
         )
 
         # 修飾キーの組み合わせを正しく変換できる
         # ついでに, 大文字小文字問わないことと, 空白有っても良いことも確認.
-        self.assertEntry(
+        assert_entry(
             testname + "," + "cs, k", testname,
             win32con.MOD_CONTROL|win32con.MOD_SHIFT,
             keycode.K
         )
-        self.assertEntry(
+        assert_entry(
             testname + "," + "   \t cWa   \t , k \t\t  ", testname,
             win32con.MOD_CONTROL|win32con.MOD_WIN|win32con.MOD_ALT,
             keycode.K
         )
-        self.assertEntry(
+        assert_entry(
             testname + "," + "aSCw, k", testname,
             win32con.MOD_SHIFT|win32con.MOD_CONTROL| \
             win32con.MOD_WIN|win32con.MOD_ALT,
