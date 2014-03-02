abece2840a9ea50d4517b598e3953abc19ed76e1
shouh <shouhpublic@gmail.com>
2014-03-02 10:50:33 +0900

    公開前準備 説明書更新, 公開用hotkey.ini, バージョン番号更新

--- instanttextinserter/build.bat
+++ instanttextinserter/build.bat
@@ -6,6 +6,7 @@ rmdir /s /q .\build
 xcopy .\license .\dist\license /I
 xcopy .\for_dist\snippet .\dist\snippet /I
 copy .\for_dist\readme.txt .\dist\readme.txt
+copy .\for_dist\hotkey.ini .\dist\hotkey.ini
 ren .\dist\entrypoint.exe iti.exe
 
 python -c "import os; import selfinfo; os.rename('dist',selfinfo.PROGRAM_NAME + selfinfo.VERSION)"
