15e1d6f1fccafbf69c0215a3166a1fea7c037c4b
shouh <dummy@dummymailaddress.com>
2015-04-28 11:14:07 +0900

    v0.0.6用微修正

--- instanttextinserter/selfinfo.py
+++ instanttextinserter/selfinfo.py
@@ -5,7 +5,7 @@ import sys
 
 # Program Information
 PROGRAM_NAME = "InstantTextInserter"
-VERSION = "0.0.5"
+VERSION = "0.0.6"
 AUTHOR = "shouh"
 PROGRAM_INFO = PROGRAM_NAME + " " + "v" + VERSION + " (c) " + AUTHOR
 
