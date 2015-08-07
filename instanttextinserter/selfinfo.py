c8d2e9a8cf45b02158524d3145396cbef1b476e3
shouh <dummy@dummymailaddress.com>
2015-08-07 13:47:08 +0900

    バージョン番号更新 0.0.7->0.1.0 and open対応分のドキュメントを更新

--- instanttextinserter/selfinfo.py
+++ instanttextinserter/selfinfo.py
@@ -5,7 +5,7 @@ import sys
 
 # Program Information
 PROGRAM_NAME = "InstantTextInserter"
-VERSION = "0.0.7"
+VERSION = "0.1.0"
 AUTHOR = "shouh"
 PROGRAM_INFO = PROGRAM_NAME + " " + "v" + VERSION + " (c) " + AUTHOR
 
